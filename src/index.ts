9087d41a4b15f4d8ef0c82678ada547ee47eb4e2
Sergey Bevzenko <sabevzenko@avito.ru>
2024-02-21 00:21:21 +0100

    wip: opensearch

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,39 @@
+import { Client } from '@opensearch-project/opensearch';
 import express, { Request, Response , Application } from 'express';
 import dotenv from 'dotenv';
 
 //For env File 
 dotenv.config();
 
+
+const client = new Client({
+  node: 'https://admin:admin@localhost:9200'
+});
+
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/',async (req: Request, res: Response) => {
+  console.log('Creating index:');
+
+  const index_name = 'books';
+  const settings = {
+    settings: {
+      index: {
+        number_of_shards: 4,
+        number_of_replicas: 3,
+      },
+    },
+  };
+  
+  const response = await client.indices.create({
+    index: index_name,
+    body: settings,
+  });
+  
+  
+  console.log(response.body);
+
   res.send('Welcome to Express & TypeScript Server!!!');
 });
 
